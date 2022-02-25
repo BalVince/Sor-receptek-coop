437b6f730604dc2cc305cea8b29f6d54a67052dc
Balogh Vince <baloghvince.bv@gmail.com>
2022-02-25 13:05:44 +0100

    asdf

--- script.js
+++ script.js
@@ -12,20 +12,28 @@ async function main(){
     for (const key in szotar) {
         let gomb = document.createElement("button");
         gomb.setAttribute("class", "sorgomb");
-        gomb.onclick = milegyen;
-        gomb.setAttribute("id", key);
+        gomb.onclick = (e)=>{milegyen(e, szotar[key])};
         gomb.innerHTML = szotar[key]["name"];
         sorgombdiv.appendChild(gomb);
     }
-
-   
-
 }
 
-function milegyen(e){
-    alert("ááááááááááá");
-    console.log(e.target);
-}
+function milegyen(e, szotarelement){
+    let temp = document.getElementById("sornevtagline");
+    temp.innerHTML = szotarelement["name"] + " - " + szotarelement.tagline;
+    
+    temp = document.getElementById("ido");
+    temp.innerHTML = "First brewed: " + szotarelement.first_brewed;
+
+    temp = document.getElementById("desc");
+    temp.innerHTML = szotarelement.description;
+
+    temp = document.getElementById("menny");
+    temp.innerHTML = `Volume: ${szotarelement.volume.value} ${szotarelement.volume.unit} (Boil volume: ${szotarelement.boil_volume.value} ${szotarelement.boil_volume.unit})` 
+
+    temp = document.getElementById("cefre");
+    temp.innerHTML = `Mashing: ${szotarelement.method.mash_temp[0].temp.value}`
+}[
 
 async function olvaso_fetch(url){
     let promise = await fetch(url);
